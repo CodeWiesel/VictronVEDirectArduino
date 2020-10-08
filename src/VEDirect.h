91707a90fd2802a65f5362d53934ff7f3167657a
jimwsmith <42283187+jimwsmith@users.noreply.github.com>
2020-10-08 09:19:35 +0100

    Update VEDirect.h

--- src/VEDirect.h
+++ src/VEDirect.h
@@ -2,6 +2,7 @@
  VEDirect Arduino
 
  Copyright 2018, 2019, Brendan McLearie
+ Modified 2020, Jim Smith
  Distributed under MIT license - see LICENSE.txt
 
  See README.md
@@ -10,6 +11,8 @@
  - Class / enums / API
 
  Updates:
+ - 2020-10--8:
+ 		Added additional labels to read MPPT devices
  - 2019-07-14:
   	  - Rewrite of read - cleaner.
   	  - Target labels extendible with enum and PROGMEM strings
@@ -23,21 +26,40 @@
 #include <Arduino.h>
 
 // Tunable parameters - defaults tested on mega2560 R3
-#define VED_LINE_SIZE 30		 // Seems to be plenty. VE.Direct protocol could change
-#define VED_MAX_LEBEL_SIZE 6	 // Max length of all labels of interest + '\0'. See ved_labels[]
-#define VED_MAX_READ_LOOPS 60000 // How many read loops to be considered a read time-out
-#define VED_MAX_READ_LINES 50	 // How many lines to read looking for a value
-								 // before giving up. Also determines lines for diag dump
+#define VED_LINE_SIZE 30	// Seems to be plenty. VE.Direct protocol could change
+#define VED_MAX_LEBEL_SIZE 10	 // Max length of all labels of interest + '\0'. See ved_labels[]
+#define VED_MAX_READ_LOOPS 6000 // How many read loops to be considered a read time-out
+#define VED_MAX_READ_LINES 100	 // How many lines to read looking for a value
+				// before giving up. Also determines lines for diag dump
 #define VED_BAUD_RATE 19200
 
 // Extend this and ved_labels[] for needed inclusions
 enum VE_DIRECT_DATA {
 	VE_DUMP = 0,
-	VE_SOC,
-	VE_VOLTAGE,
-	VE_POWER,
-	VE_CURRENT,
-	VE_ALARM,
+	VE_BMV_SOC,
+	VE_BMV_VOLTAGE,
+	VE_BMV_STARTV,
+	VE_BMV_POWER,
+	VE_BMV_CURRENT,
+	VE_BMV_ALARM,
+	VE_BMV_RELAY,
+	VE_MPPT_VOLTAGE,
+	VE_MPPT_PANELV,
+	VE_MPPT_POWER,
+	VE_MPPT_CURRENT,
+	VE_MPPT_ALARM,
+	VE_MPPT_STATE,
+	VB_DC_VBATT,		//VE BUS readings via USB port
+	VB_DC_IINV,
+	VB_DC_ICHG,
+	VB_DC_PBATT,
+	VB_AC_VMAINS,
+	VB_AC_IMAINS,
+	VB_AC_PMAINS,
+	VB_AC_VINV,
+	VB_AC_IINV,
+	VB_AC_PINV,
+	VB_LED_STATE,
 	VE_LAST_LABEL,
 };
 
@@ -45,9 +67,17 @@ const char ved_labels[VE_LAST_LABEL][VED_MAX_LEBEL_SIZE] PROGMEM = {
 		"Dump",	// a string that won't match any label
 		"SOC",
 		"V",
+		"VS",
 		"P",
 		"I",
 		"Alarm",
+		"Relay",
+		"V",
+		"VPV",
+		"PPV",
+		"I",
+		"Alarm",
+		"CS",
 };
 
 class VEDirect {
